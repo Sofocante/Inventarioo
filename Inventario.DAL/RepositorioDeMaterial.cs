867ea85c8dd085b818bac8c41de323e5c6e10cbb
EDUARDO YAHIR OROPEZA <eduardoyahir.oropeza875@alumno.cch.unam.mx>
2021-05-21 17:29:54 -0500

    Repositorios Vale y Material terminados

---
+++ Inventario.DAL/RepositorioDeMaterial.cs
@@ -0,0 +1,84 @@
+﻿using Inventarioo.COMMON.Entidades;
+using Inventarioo.COMMON.Interfaces;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inventario.DAL
+{
+    public class RepositorioDeMaterial : IRepositorio<Material>
+    {
+        private string DBName = "Inventario.db";
+        private string TableName = "Material";
+
+        public List<Material> Read {
+            get
+            {
+                List<Material> datos = new List<Material>();
+                using (var db = new LiteDatabase(DBName))
+                {
+                    datos = db.GetCollection<Material>(TableName).FindAll().ToList();
+                }
+                return datos;
+            }
+
+        }
+        public bool Create(Material entidad)
+        {
+            entidad.Id = Guid.NewGuid().ToString();
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Material>(TableName);
+                    coleccion.Insert(entidad);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(string id, Material entidad)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Material>(TableName);
+                    coleccion.Delete(entidad.Id == id);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Material entidadModificada)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Material>(TableName);
+                    coleccion.Insert(entidadModificada);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Inventario.DAL/RepositorioDeVale.cs
@@ -0,0 +1,84 @@
+﻿using Inventarioo.COMMON.Entidades;
+using Inventarioo.COMMON.Interfaces;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inventario.DAL
+{
+    public class RepositorioDeVale : IRepositorio<Vale>
+    {
+        private string DBName = "Inventario.db";
+        private string TableName = "Vale";
+
+        public List<Vale> Read {
+            get
+            {
+                List<Vale> datos = new List<Vale>();
+                using (var db = new LiteDatabase(DBName))
+                {
+                    datos = db.GetCollection<Vale>(TableName).FindAll().ToList();
+                }
+                return datos;
+            }
+
+        }
+        public bool Create(Vale entidad)
+        {
+            entidad.Id = Guid.NewGuid().ToString();
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Vale>(TableName);
+                    coleccion.Insert(entidad);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public bool Delete(string id, Vale entidad)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Vale>(TableName);
+                    coleccion.Delete(entidad.Id == id);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Vale entidadModificada)
+        {
+            try
+            {
+                using (var db = new LiteDatabase(DBName))
+                {
+                    var coleccion = db.GetCollection<Vale>(TableName);
+                    coleccion.Insert(entidadModificada);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
